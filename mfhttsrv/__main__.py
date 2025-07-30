fe4a825577f8c78b612c9294642b54c0c6998f7c
Toshiyuki Ogawa <toshi@oc-soft.net>
2025-07-31 08:39:57 +0900

    I had a simple http server.

--- mfhttsrv/__main__.py
+++ mfhttsrv/__main__.py
@@ -1,15 +1,50 @@
-import http.server
+from http import server
+# httpパッケージからserverモジュールをインポート
 import socket
+# socketモジュールをインポート
 from .request.handler import Handler
+# 自作Httpリクエスト応答モジュールをインポート
 
 
+# httpサーバが待ち受けするポートを指定
 port_number = 8000
 
-addres_info_list = socket.getaddrinfo(None, port_number)
+# 稼働システムの全てのアドレスで待ち受け(スマホからテストするため)
+bind_address = '0.0.0.0'
+
+# 稼働システムのport_numberに対するアドレス一覧を得る
+address_info_list = socket.getaddrinfo(bind_address, port_number)
+
+# http リクエストの対応バージョンを1.0とする。
+Handler.protol_version = 'HTTP/1.0'
 
 
+# address_info_listの一番最初が、streamになっている。
+# streamがhttpサーバが対応するデータタイプ
 
-# http.server.ThreadingHttpServer.address
+# 取得したアドレス最初のデータを各種設定に使用する
+address_info = address_info_list[0]
 
+# 通信のアドレスファミリをipに設定
+server.ThreadingHTTPServer.address_familiy = address_info[0]
+
+# http サーバインスタンスを作成
+# address_info[1]は、サーバアドレス
+# Handlerは、httpリクエストを処理するクラス
+with server.ThreadingHTTPServer(address_info[4], Handler) as httpd:
+    # httpサーバインスタンスが実際に待ち受けしているホスト名とポート番号を取得
+    host, port = httpd.socket.getsockname()[:2] 
+    print(f'Serving HTTP on port {port}')
+    try:
+        # サービス開始
+        httpd.serve_forever()
+        # これ以降の処理は実施されない  
+        print('finished server')
+        # この処理は実行されない
+    except KeyboardInterrupt:
+        # コンソールでCtrl+Cを押下するとこの処理が実行される。
+        print('Kyeboad interrupt received, exiting.') 
+# ここに処理がくるのは、コンソールでCtrl+Cを押下した場合
+print('service is closed')
 
 # vi: se ts=4 sw=4 et:
--- mfhttsrv/request/handler.py
+++ mfhttsrv/request/handler.py
@@ -1,9 +1,14 @@
-from http.server import SimpleHTTPReqesHandler
+from http import server # httpパッケージからserverをインポート
 
-class Handler(SimpleHTTPRequestHandler):
-
+class Handler(server.SimpleHTTPRequestHandler):
+    """ httpリクエストを処理する """
 
     def do_GET(self):
+        """ http リクエストのGETメソッドに応答する """
+
+        # server.SimpleHTTPRequestHandler.do_GETに処理を委譲する。
         super().do_GET()  
+        pass
+
     pass
 # vi: se ts=4 sw=4 et:
