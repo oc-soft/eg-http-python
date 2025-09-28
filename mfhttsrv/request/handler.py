ebeee2014d8b10b5d51363b23da81397ebb0fe1c
Toshiyuki Ogawa <toshiyuki.ogawa.oc@gmail.com>
2025-09-29 07:27:41 +0900

    I adjust width for small display width device.

--- mfhttsrv/request/handler.py
+++ mfhttsrv/request/handler.py
@@ -146,7 +146,28 @@ class Handler(server.SimpleHTTPRequestHandler):
                     /* 完全な白ではなく非常に薄いグレーを表示に使用 */
                     color: #cccccc;
                 }}
-          }}
+            }}
+            @media (width <= 450px) {{
+                main table {{
+                    display: block;
+                }}
+                main table tr {{
+                    display: block;
+                    margin-top: 10px;
+                }}
+                main table tr:first-element {{
+                    margin-top: 0px;
+                }}
+                main table tr td {{
+                    display: block;
+                }}
+            }}
+            @media (width <= 380px) {{
+                body {{
+                    font-size: 0.8em;
+                }}
+            }}
+
         </style>
         <script>
             /**
